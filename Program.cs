da420517ba769f37dc84a74245d5087a5560001f
GabrielaGonzaga <73176744+GabrielaGonzaga@users.noreply.github.com>
2020-11-18 09:29:31 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace While
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Que tabuda você gostaria de saber?");
+            
+            int tabuada = int.Parse(Console.ReadLine());
+            
+            int contador = 0;
+            
+            while(contador<=10){
+                Console.WriteLine(tabuada * contador);
+                contador = contador + 1;
+                
+            }
+
+        }
+    }
+}
